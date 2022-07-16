0fe1fb1cdad54c5e80729b6b40185774992e4de9
Roman Kazarov <rkazarov@siriusiq.com>
2022-07-16 13:12:17 +0300

    Add html parser

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type TopCustomer struct {
@@ -14,7 +18,28 @@ type TopCustomer struct {
 func main() {
 	html := getHtml()
 	defer html.Close()
+	topCustomers := parseHtml(html)
 
+}
+
+func parseHtml(html io.ReadCloser) []TopCustomer {
+	doc, err := goquery.NewDocumentFromReader(html)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topCustomers := []TopCustomer{}
+	doc.Find("table.summary td").Each(func(index int, selection *goquery.Selection) {
+		selectionAttribute, ok := selection.Attr("x-total-candy")
+		if ok {
+			selectionText := selection.Text()
+			total, _ := strconv.Atoi(selectionAttribute)
+			topCustomers = append(topCustomers, TopCustomer{Name: selectionText, TotalSnacks: total})
+		} else {
+			topCustomers[len(topCustomers)-1].FavouriteSnack = selection.Text()
+		}
+	})
+	return topCustomers
 }
 
 func getHtml() io.ReadCloser {
