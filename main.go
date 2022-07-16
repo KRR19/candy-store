65acdb49092be4bf63e20e798badc52e31cb5a3b
Roman Kazarov <rkazarov@siriusiq.com>
2022-07-16 13:04:59 +0300

    Add top customer  struct

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+type TopCustomer struct {
+	Name           string `json:"name"`
+	FavouriteSnack string `json:"favouriteSnack"`
+	TotalSnacks    int    `json:"totalSnacks"`
+}
+
 func main() {
 	html := getHtml()
 	defer html.Close()
+
 }
 
 func getHtml() io.ReadCloser {
